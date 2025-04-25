ec641be7e8fd3725cf3fd8a2d2984e3bc531689a
Isomorphism <jw6699@nyu.edu>
2025-04-25 13:41:46 -0400

    util class for parsing strings and record result in a linked list like structure

---
+++ src/main/java/org/isomorphism/backend/utils/ParseStringRes.java
@@ -0,0 +1,13 @@
+package org.isomorphism.backend.utils;
+
+public class ParseStringRes {
+
+    public String str;
+    public int next;
+
+    public ParseStringRes(String str, int next) {
+        this.str = str;
+        this.next = next;
+    }
+
+}
