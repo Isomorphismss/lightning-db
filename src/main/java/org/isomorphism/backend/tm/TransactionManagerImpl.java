fdd8f13d6e82ae22cee23c1a8a6598e2ca327386
Isomorphism <jw6699@nyu.edu>
2025-04-25 14:13:46 -0400

    added functions to add/incr xid and check file validity in TransactionManagerImpl

--- src/main/java/org/isomorphism/backend/tm/TransactionManagerImpl.java
+++ src/main/java/org/isomorphism/backend/tm/TransactionManagerImpl.java
@@ -1,5 +1,16 @@
 package org.isomorphism.backend.tm;
 
+import org.isomorphism.backend.utils.Panic;
+import org.isomorphism.backend.utils.Parser;
+import org.isomorphism.common.Error;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TransactionManagerImpl implements TransactionManager {
 
     // XID文件头长度
@@ -19,9 +30,100 @@ public class TransactionManagerImpl implements TransactionManager {
     // XID 文件后缀
     static final String XID_SUFFIX = ".xid";
 
+    private RandomAccessFile file;
+    private FileChannel fc;
+    private long xidCounter;
+    private Lock counterLock;
+
+    public TransactionManagerImpl(RandomAccessFile raf, FileChannel fc) {
+        this.file = raf;
+        this.fc = fc;
+        counterLock = new ReentrantLock();
+        checkXIDCounter();
+    }
+
+    /**
+     * 检查XID文件是否合法
+     * 读取XID_FILE_HEADER中的xidcounter，根据它计算文件的理论长度，对比实际长度
+     */
+    private void checkXIDCounter() {
+        long fileLen = 0;
+        try {
+            fileLen = file.length();
+        } catch (IOException e1) {
+            Panic.panic(Error.BadXIDFileException);
+        }
+        if(fileLen < LEN_XID_HEADER_LENGTH) {
+            Panic.panic(Error.BadXIDFileException);
+        }
+
+        ByteBuffer buf = ByteBuffer.allocate(LEN_XID_HEADER_LENGTH);
+        try {
+            fc.position(0);
+            fc.read(buf);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+        this.xidCounter = Parser.parseLong(buf.array()); // 当前的最大xid
+        long end = getXidPosition(this.xidCounter + 1); // 加上一直有的这个xid 0
+        if (end != fileLen) {
+            Panic.panic(Error.BadXIDFileException);
+        }
+    }
+
+    // 根据事务xid取得其在xid文件中对应的位置
+    private long getXidPosition(long xid) {
+        return LEN_XID_HEADER_LENGTH + (xid - 1) * XID_FIELD_SIZE;
+    }
+
+    // 更新xid事务的状态为status
+    private void updateXID(long xid, byte status) {
+        long offset = getXidPosition(xid);
+        byte[] tmp = new byte[XID_FIELD_SIZE];
+        tmp[0] = status;
+        ByteBuffer buf = ByteBuffer.wrap(tmp);
+        try {
+            fc.position(offset);
+            fc.write(buf);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+        try {
+            fc.force(false); // 直接刷盘
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+    }
+
+    // 将xid加一，并更新xid header
+    private void incrXIDCounter() {
+        xidCounter++;
+        ByteBuffer buf = ByteBuffer.wrap(Parser.long2Byte(xidCounter));
+        try {
+            fc.position(0);
+            fc.write(buf);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+        try {
+            fc.force(false);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+    }
+
+    // 开始一个事务，返回xid
     @Override
     public long begin() {
-        return 0;
+        counterLock.lock();
+        try {
+            long xid = xidCounter + 1;
+            updateXID(xid, FIELD_TRAN_ACTIVE);
+            incrXIDCounter();
+            return xid;
+        } finally {
+            counterLock.unlock();
+        }
     }
 
     @Override
@@ -53,4 +155,5 @@ public class TransactionManagerImpl implements TransactionManager {
     public void close() {
 
     }
+
 }
